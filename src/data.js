fb07afd3c98b584be5f111f94b433b22f497f9aa
Luyao <smmileely@gmail.com>
2021-06-09 16:25:17 +0800

    Add data information

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const ExperienceData = {
+  header: "Experience",
+  lead: "Quantity Surveyor",
+  company: "Rider Levett Bucknall",
+  date: "Aug 2019 - June 2021",
+  description: ["qwerty", "werty"],
+};
