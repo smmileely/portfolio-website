86fda05b4ab5158f4fe44aa56e426e88f34e9fcd
Luyao <smmileely@gmail.com>
2021-06-16 00:49:25 +0800

    Updated sidebar

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { COLORS, WEIGHTS } from "../constants";
-import { FaTimes, FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 const Sidebar = ({ isOpen, toggle }) => {
   return (
@@ -35,40 +35,55 @@ const Sidebar = ({ isOpen, toggle }) => {
 const SidebarContainer = styled.aside`
   display: none;
 
-  @media (max-width: 960px) {
+  @media screen and (max-width: 768px) {
     position: fixed;
-    z-index: 999;
+    z-index: 999; //above everything
     width: 100%;
     height: 100%;
     background: white;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    transition: 0.3s ease-in-out;
+    opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+    top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
   }
-`;
-
-const Icon = styled(FaBars)`
-  position: absolute;
-  color: ${COLORS.secondary};
-  cursor: pointer;
 `;
 
 const CloseIcon = styled(FaTimes)`
   color: ${COLORS.secondary};
 `;
 
+const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`;
+
 const SidebarWrapper = styled.div`
   background: white;
 `;
 
-const SidebarMenu = styled.div`
+const SidebarMenu = styled.ul`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  gap: 50px;
 `;
 
 const SidebarLink = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  cursor: pointer;
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
@@ -78,6 +93,7 @@ const SidebarLink = styled.a`
   &:hover {
     /* text-decoration: revert; */
     font-weight: ${WEIGHTS.bold};
+    transition: 0.2s ease-in-out;
   }
 `;
 
