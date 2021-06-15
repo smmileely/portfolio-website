6505f5773be05f7389a425dafc95b5f3790dff25
Luyao <smmileely@gmail.com>
2021-06-16 00:48:52 +0800

    Updated Navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,38 +1,74 @@
 import React from "react";
 import styled from "styled-components";
 import { COLORS, WEIGHTS } from "../constants";
+import { FaBars } from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
-    <NavbarContainer>
-      <Nav>
-        <NavLink href="/home">Home</NavLink>
-        <NavLink href="/profile">Profile</NavLink>
-        <NavLink href="/skills">Skills</NavLink>
-        <NavLink href="/experience">Experience</NavLink>
-        <NavLink href="/education">Education</NavLink>
-      </Nav>
-    </NavbarContainer>
+    <Nav>
+      <NavbarContainer>
+        <MobileIcon onClick={toggle}>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          <NavLink href="/home">Home</NavLink>
+          <NavLink href="/profile">Profile</NavLink>
+          <NavLink href="/skills">Skills</NavLink>
+          <NavLink href="/experience">Experience</NavLink>
+          <NavLink href="/education">Education</NavLink>
+        </NavMenu>
+      </NavbarContainer>
+    </Nav>
   );
 };
 
-const NavbarContainer = styled.div`
+const Nav = styled.nav`
+  background: white;
+  height: 80px;
   display: flex;
-  align-items: baseline;
+  align-items: center;
   justify-content: flex-end;
-  padding: 20px 40px 20px 0px;
+  position: sticky;
 
   @media (max-width: 960px) {
-    /* transition: 0.8s all ease; */
-    display: none;
+    transition: 0.8s all ease;
   }
 `;
 
-const Nav = styled.nav`
+const NavbarContainer = styled.div`
   display: flex;
   align-items: center;
+  height: 80px;
+  padding: 20px 40px;
   gap: 48px;
-  height: 100%;
+  max-width: 1100px;
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    position: absolute;
+    top: 10px;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.5rem;
+    cursor: pointer;
+    color: ${COLORS.secondary};
+  }
+`;
+
+const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  gap: 48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavLink = styled.a`
