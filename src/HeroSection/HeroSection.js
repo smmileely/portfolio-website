3cdbc69dc3746b6225923573091d7031ba3af8dc
Luyao <smmileely@gmail.com>
2021-06-16 17:01:38 +0800

    Updated herosection with background image

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -1,28 +1,122 @@
 import React from "react";
 import styled from "styled-components";
+import HeroSectionImage from "../images/HeroSectionImage.JPG";
+import { COLORS, WEIGHTS } from "../constants";
 
 const HeroSection = () => {
   return (
     <HeroContainer>
-      <HeroContent>Hello</HeroContent>
+      <HeroBg>
+        <ImgBg src={HeroSectionImage} />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Weng Lu Yao</HeroH1>
+        <HeroP>
+          A quantity surveyor who is trying to learn new skills and venture into
+          the world of codes.
+        </HeroP>
+      </HeroContent>
     </HeroContainer>
   );
 };
 
-const HeroContainer = styled.div`
-  background-image: url("../images/HeroSectionImage.JPG");
-  background-size: cover;
-  background-position: bottom center;
-  height: 800px;
+export const HeroContainer = styled.div`
+  background: #0c0c0c;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 0 30px;
+  height: 800px;
   position: relative;
-  z-index: 1; //for other components in Herosection
+  z-index: 1;
+
+  /* :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.2) 0%,
+        rgba(0, 0, 0, 0.6) 100%
+      ),
+      linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+    z-index: 2;
+  } */
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const ImgBg = styled.img`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
 `;
 
 const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  padding: 8px 24px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
+
+const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  /* @media screen and (max-width: 768 px) {
+    font-size: 40px;
+  }
+  @media screen and (max-width: 480 px) {
+    font-size: 32px; */
+  }
+`;
+
+const HeroP = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  /* @media screen and (max-width: 768 px) {
+    font-size: 24px;
+  }
+  @media screen and (max-width: 480 px) {
+    font-size: 18px; */
+  }
+`;
+
+// const HeroContainer = styled.div`
+//   background-image: url("../images/HeroSectionImage.JPG");
+//   background-size: cover;
+//   background-position: bottom center;
+//   height: 800px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   position: relative;
+//   z-index: 1; //for other components in Herosection
+// `;
+
+// const HeroContent = styled.div`
+//   display: flex;
+// `;
 
 export default HeroSection;
