c20a7b96e0709245dd918fea683fd5154b653d5b
Luyao <smmileely@gmail.com>
2021-06-21 02:37:05 +0800

    Cleaned up Navbar code

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { COLORS, WEIGHTS } from "../constants";
 import { FaBars } from "react-icons/fa";
