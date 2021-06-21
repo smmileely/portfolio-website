9db33ccaab09bd2b5db61a7a7690956674b1198c
Chen Jie <e0036922@u.nus.edu>
2021-06-21 17:45:34 +0800

    fix hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import Sidebar from "./Sidebar";
-import Navbar from "./Navbar";
-import HeroSection from "./HeroSection";
-import Profile from "./Profile";
-import Skills from "./Skills";
-import InfoCard from "./InfoCard";
-import Footer from "./Footer";
-import { ExperienceData, EducationData } from "./data";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import HeroSection from './HeroSection';
+import Profile from './Profile';
+import Skills from './Skills';
+import InfoCard from './InfoCard';
+import Footer from './Footer';
+import { ExperienceData, EducationData } from './data';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,8 +25,8 @@ const App = () => {
         <Profile />
         <Skills />
         <ContentWrapper>
-          <InfoCard header="Experience" data={ExperienceData} />
-          <InfoCard header="Education" data={EducationData} />
+          <InfoCard header='Experience' data={ExperienceData} />
+          <InfoCard header='Education' data={EducationData} />
         </ContentWrapper>
       </MiddleSection>
       <Footer />
