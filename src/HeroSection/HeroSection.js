e52480c2f9310da621544679c0312b086a0700dd
Luyao <smmileely@gmail.com>
2021-06-28 03:15:05 +0800

    Cleaned up codes

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-// import HeroSectionImage from "/assets/HeroSectionImage.JPG";
-
 import { COLORS, FONTS } from "../constants";
 
 const HeroSection = () => {
