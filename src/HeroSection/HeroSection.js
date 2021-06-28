9b77195b2bc237edcd278e7378f01bc812a6d38a
Luyao <smmileely@gmail.com>
2021-06-29 00:39:05 +0800

    Updated mobile query

--- src/HeroSection/HeroSection.js
+++ src/HeroSection/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
   return (
     <HeroContainer id='home'>
       <HeroImage1 src='/assets/HeroSectionImage1.JPG' alt='My hero picture' />
-      <HeroImage2 src='/assets/HeroSectionImage2.JPG' alt='My hero picture' />
+      <HeroImage2 src='/assets/IMG_8815.JPG' alt='My hero picture' />
       <HeroH1>Hi there, I'm Lu Yao.</HeroH1>
       <HeroH2>
         A quantity surveyor who is trying to learn new skills and venture into
@@ -30,13 +30,13 @@ const HeroContainer = styled.div`
   align-items: center;
 
   text-align: center;
-  background: -webkit-linear-gradient(
+  /* background: -webkit-linear-gradient(
     0deg,
     ${COLORS.gray[900]},
     ${COLORS.gray[100]}
   );
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+  -webkit-text-fill-color: transparent; */
 `;
 
 const HeroImage1 = styled.img`
@@ -50,11 +50,28 @@ const HeroImage2 = styled.img`
 
   @media (max-width: 768px) {
     display: block;
+    max-width: 100%;
+    height: auto;
+    margin-top: -64px;
+    overflow: hidden;
   }
 `;
 
 const HeroH1 = styled.h1`
   font-size: ${FONTS.title};
+
+  @media (max-width: 768px) {
+    position: absolute;
+    z-index: 999;
+    margin: 0 auto;
+    left: 14px;
+    right: 0;
+    top: 46%;
+    text-align: left;
+    width: 100%;
+    color: black;
+    font-size: ${FONTS.heading};
+  }
 `;
 
 const HeroH2 = styled.h2`
